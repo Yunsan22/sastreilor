b4a4a32cdfcd66012fa89a3c463b25d0019dff41
yunior sanchez <ygar0612@fmuniv.edu>
2022-12-07 08:50:58 -0500

    adding Signin/Signup activities

--- app/src/main/java/com/example/sastreilor/MainActivity.kt
+++ app/src/main/java/com/example/sastreilor/MainActivity.kt
@@ -2,9 +2,17 @@ package com.example.sastreilor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Obtain the FirebaseAnalytics instance.
+        firebaseAnalytics = Firebase.analytics
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
