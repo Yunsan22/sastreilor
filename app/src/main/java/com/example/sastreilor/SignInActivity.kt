b33fa28a926df465bd4e3d38b449068345db5692
yunior sanchez <ygar0612@fmuniv.edu>
2022-12-08 08:25:46 -0500

    adding Signin/Signup activities layouts

--- app/src/main/java/com/example/sastreilor/SignInActivity.kt
+++ app/src/main/java/com/example/sastreilor/SignInActivity.kt
@@ -1,11 +1,29 @@
 package com.example.sastreilor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sastreilor.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //hide action bar
+        supportActionBar?.hide()
+
+        //this init Firebase
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.createacctTexview.setOnClickListener {
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/sastreilor/SignUpActivity.kt
+++ app/src/main/java/com/example/sastreilor/SignUpActivity.kt
@@ -1,11 +1,28 @@
 package com.example.sastreilor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sastreilor.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //hide action bar
+        supportActionBar?.hide()
+
+
+        //this init Firebase
+        firebaseAuth = FirebaseAuth.getInstance()
+        // the below textview sends the user to sign in activity
+        binding.signinTexview.setOnClickListener {
+            val intent = Intent(this,SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
